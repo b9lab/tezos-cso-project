f1b86dfa6a2f08b8e42a2a33d4e554415056fc73
Gioele Bonifetto <gioele@b9lab.com>
2021-06-25 17:14:27 +0200

    implements basic home layout for demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-center">Tezos CSO App</h1>
+    <div className="max-w-screen-lg m-auto">
+      <h1 className="text-center mb-4">Tezos CSO App</h1>
+      <div className="w-full h-80 bg-yellow-400"></div>
+      <div className="flex flex-wrap">
+        <div className="h-80 w-full bg-green-400 lg:w-1/2">
+        
+        </div>
+        <div className="h-80 w-full bg-red-400 lg:w-1/2">
+        
+        </div>
+      </div>
     </div>
   )
 }
