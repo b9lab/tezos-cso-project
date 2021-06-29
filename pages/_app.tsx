efe1fcbe2b2960e0aa0ba72ce69c01e7391ccd7a
Gioele Bonifetto <gioele@b9lab.com>
2021-06-29 22:01:04 +0200

    fixes build error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,10 @@ import Auth from '../src/components/Auth';
 
 const fetcher = (input: RequestInfo, init: RequestInit) => fetch(input, init).then(res => res.json());
 
-function App({ Component, pageProps }: AppProps) {
+function App(props: AppProps) {
+  const Component: any = props.Component;
+  const pageProps = props.pageProps;
+  
   return (
     <SWRConfig value={{fetcher: fetcher}}>
       <Layout>
