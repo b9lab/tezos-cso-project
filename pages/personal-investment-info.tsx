203a2565dfa2c43727452f9fd62cd9056ab6f8b4
Gioele Bonifetto <gioele@b9lab.com>
2021-07-01 18:04:29 +0200

    add content to personal investment info page

--- pages/personal-investment-info.tsx
+++ pages/personal-investment-info.tsx
@@ -1,9 +1,18 @@
 import Link from "next/link";
+import useSWR from "swr";
 
 export default function PersonalInvestmentInfo() {
+    const { data } = useSWR("api/users/investment-data");
+    
     return (
         <>
-            <div className="mb-4">Personal Investment Info page</div>
+            <div className="mb-4 font-bold">Personal Investment Info page</div>
+            <div className="mb-4">
+                tez invested: {data?.tezInvested} <br/>
+                tokens owned: {data?.tokensOwned} <br/>
+                token buy price: {data?.tokenBuyPrice} <br/>
+                token sell price: {data?.tokenSellPrice}
+            </div>
             <Link href="/fund-withdraw">
                 <div className="w-full h-80 bg-purple-400 cursor-pointer p-4 text-white text-3xl">Fund / Withdraw</div>
             </Link>
@@ -13,3 +22,5 @@ export default function PersonalInvestmentInfo() {
         </>
     );
 }
+
+PersonalInvestmentInfo.auth = true;
