687cdb3f61028b61ea070fb63ffd856007dc6493
Gioele Bonifetto <gioele@b9lab.com>
2021-07-12 17:12:45 +0200

    implements highlight style

--- pages/feature-test.tsx
+++ pages/feature-test.tsx
@@ -23,6 +23,13 @@ export default function Page() {
             </div>
 
             <div className="my-4">
+                <h2 className="highlight">Subheadline H2 with highlight</h2>
+                <div>
+                    Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.
+                </div>
+            </div>
+
+            <div className="my-4">
                 <h2 className="mb-4">Form Input</h2>
                 <Input value="Text input text" handler={() => {}} label="Fieldname"/>
             </div>
