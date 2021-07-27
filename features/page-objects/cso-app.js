4a0879dfb4a1aa806bc8f22d1750755f78c6c0b8
Gioele Bonifetto <gioele@b9lab.com>
2021-07-27 16:44:51 +0200

    implements protected pages tests

--- features/page-objects/cso-app.js
+++ features/page-objects/cso-app.js
@@ -3,12 +3,19 @@ module.exports = {
     url: 'http://localhost:3000/',
 
     paths: {
-        investmentInfo: "general-investment-info"
+        investmentInfo: "general-investment-info",
+        profile: "profile",
+        login: "api/auth/signin",
+        myInvestment: "personal-investment-info",
+        fund: "fund",
+        withdraw: "withdraw",
+        transactions: "transactions"
     },
 
     selectors: {
         title: 'main h1.text-center',
-        investmentAmounts: 'h1 > .tez-amount > .amount'
+        investmentAmounts: 'h1 > .tez-amount > .amount',
+        loginPage: '.page .signin'
     },
 
     amountsNotEmpty: async function() {
--- features/step-definitions/base-steps.js
+++ features/step-definitions/base-steps.js
@@ -14,4 +14,28 @@ module.exports = function () {
     this.Then(/^I should have values in all information boxes/, async function () {
         return pageObjects.csoApp.amountsNotEmpty(); 
     });
+
+    this.Given(/^I visit the profile page/, function() {
+        return helpers.loadPage(pageObjects.csoApp.url + pageObjects.csoApp.paths.profile);
+    });
+
+    this.Then(/^I should be on the login page/, async function () {
+        return sharedObjects.helper.selectorNotEmpty(pageObjects.csoApp.selectors.loginPage);
+    });
+
+    this.Given(/^I visit the my investment page/, function() {
+        return helpers.loadPage(pageObjects.csoApp.url + pageObjects.csoApp.paths.myInvestment);
+    });
+
+    this.Given(/^I visit the fund page/, function() {
+        return helpers.loadPage(pageObjects.csoApp.url + pageObjects.csoApp.paths.fund);
+    });
+
+    this.Given(/^I visit the withdraw page/, function() {
+        return helpers.loadPage(pageObjects.csoApp.url + pageObjects.csoApp.paths.withdraw);
+    });
+
+    this.Given(/^I visit the transactions page/, function() {
+        return helpers.loadPage(pageObjects.csoApp.url + pageObjects.csoApp.paths.transactions);
+    });
 }
