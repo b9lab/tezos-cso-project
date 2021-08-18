6c7a4b00de1885b7bd3687a7a2340785d90cb596
Ibo Sy <ibo@b9lab.com>
2021-08-18 15:08:02 +0200

    update sanbox deployment script

--- docker/scripts/bootstrap-sandbox.sh
+++ docker/scripts/bootstrap-sandbox.sh
@@ -2,12 +2,8 @@
 
 set -x
 
-#flobox start &
-
 echo "Bootstrap: Waiting for port 20000 to become reachable"
-#timeout 20 ash -c 'until printf "" 2>>/dev/null >>/dev/tcp/$0/$1; do sleep 1; done' localhost 20000
-
-timeout 20 sh -c 'while ! curl -s localhost:20000; do sleep 1; done'
+timeout 30 sh -c 'while ! curl -s localhost:20000; do sleep 1; done'
 
 if [ $? -eq 0 ]
 then
@@ -24,4 +20,5 @@ tezos-client import secret key alice unencrypted:edsk3QoqBuvdamxouPhin7swCvkQNgq
 tezos-client import secret key bob unencrypted:edsk3RFfvaFaxbHx8BMtEW1rKQcPtDML3LXjNqMNLCzC3wLC1bWbAt --force
 
 echo "Bootstrap: Sandbox bootstrap (2/2) - deploy contract"
-#/opt/contract/deployment.sh
+sh /opt/contract/deployment.sh
+echo "Bootstrap: Completed"
