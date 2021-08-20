945549744de57161fc6e1b5e19c77bdf377d8e13
Ibo Sy <ibo@b9lab.com>
2021-08-20 13:36:07 +0200

    update tezos app project, add package helper script

---
+++ package-dist.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+zip -r ./dist/dist.zip build database tezos-app-project knexfile.ts next-env.d.ts next.config.js package-lock.json package.json postcss.config.js server.js tailwind.config.js tsconfig.json tsconfig.knex.json
