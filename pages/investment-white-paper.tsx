9ac0675655aee4bbcf5a30bfa03a67af5c177de5
Ibo Sy <ibo@b9lab.com>
2021-08-20 16:54:46 +0200

    update whitepaper page

--- pages/investment-white-paper.tsx
+++ pages/investment-white-paper.tsx
@@ -3,10 +3,24 @@ export default function PersonalInvestmentInfo() {
         <div className="pb-8">
 
             <div className="pt-32 pb-8 px-8 bg-gradient-to-b from-light-gray to-gray-300">
-                <h1 className="mb-8">Investment White Paper</h1>
+                <h1 className="mb-8">Continuous Organizations White Paper</h1>
                 <div className="body-text-large italic">
                 </div>
-                TODO: Add link to pdf file
+            </div>
+            <div className="pt-8 px-8">
+                <div className="indent">
+                    You can find the Continuous Organizations White Paper in the <a className="font-family-body font-semibold" href="https://github.com/C-ORG/whitepaper" target="_blank">C-ORG Github Repository</a>
+                </div>
+            </div>
+
+            <div className="pt-12 px-8">
+                <h2 className="mb-8 highlight">Further Resources</h2>
+                <div className="mt-4">
+                    <ul>
+                        <li>Continuous Organizations <a className="font-family-body font-semibold" href="https://github.com/C-ORG/whitepaper#annex" target="_blank">calculation details (annex)</a></li>
+                        <li>Fairmint's <a className="font-family-body font-semibold" href="https://fairmint.co/cafe-continuous-agreement-for-future-equity" target="_blank">Continous Agreement for Future Equity</a> implementation</li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
