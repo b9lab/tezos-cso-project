172d7a9df755cf6fd5686da6a1e0e703c8381965
Gioele Bonifetto <gioele@b9lab.com>
2021-08-26 18:12:11 +0200

    exports vars before running tests

--- docker/scripts/cypress/run-tests.sh
+++ docker/scripts/cypress/run-tests.sh
@@ -2,5 +2,11 @@
 
 set -x
 
+export CONTRACT_ADDRESS=`cat /opt/shared/contract-address.txt`
+export CHAIN=private
+export NODE_PROVIDER=http://tzkt-sandbox:20000
+export INDEXER_API_ENDPOINT=http://tzkt-sync:5000
+export TEST_ACCOUNT_ADDRESS=tz1P8Sm5cLivzLYxbrad1FEZuKBEazQhriUg
+
 cd /opt/app
 npm ci && npm run test
