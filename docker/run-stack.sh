403b38d4bfca0bfa232ad14326d516615f533ee1
Gioele Bonifetto <gioele@b9lab.com>
2021-08-26 19:24:33 +0200

    fixes api endpoint

--- docker/run-stack.sh
+++ docker/run-stack.sh
@@ -4,4 +4,4 @@ set -x
 
 docker-compose -f ./sandbox.yml down -v
 chmod +x scripts/db/create-multiple-db.sh
-docker-compose -f ./sandbox.yml up
+docker-compose -f ./sandbox.yml up --abort-on-container-exit
