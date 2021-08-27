2dd85667b3e8ce7568148eb80a9b08890ce12b5c
Gioele Bonifetto <gioele@b9lab.com>
2021-08-27 17:20:25 +0200

    generic fixes

--- cypress/integration/01-protected_page.spec.js
+++ cypress/integration/01-protected_page.spec.js
@@ -69,9 +69,6 @@ describe('Protected pages tests', () => {
         it('Visiting the fund page', () => {
             cy.visit('/fund-withdraw');
             cy.get('h1').contains('Fund').should('be.visible');
-
-            // cy.get('div').contains('Sell').click();
-            // cy.get('h1').contains('Withdraw').should('be.visible');
         })
 
         it('Visiting the transactions page', () => {
--- docker/scripts/cypress/run-tests.sh
+++ docker/scripts/cypress/run-tests.sh
@@ -4,7 +4,7 @@ set -x
 
 export CONTRACT_ADDRESS=`cat /opt/shared/contract-address.txt`
 export CHAIN=private
-export NODE_PROVIDER=http://tzkt-sandbox:20000/
+export NODE_PROVIDER=http://sandbox:20000/
 export INDEXER_API_ENDPOINT=http://tzkt-api:5000/
 export TEST_ACCOUNT_ADDRESS=tz1P8Sm5cLivzLYxbrad1FEZuKBEazQhriUg
 export TEST_ACCOUNT_MNEMONIC="cargo despair tell spare victory divorce average draw source brush fancy round"
