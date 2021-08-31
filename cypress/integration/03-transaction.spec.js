e26223c6f0ccea32b81d854339b01164dc535c30
Gioele Bonifetto <gioele@b9lab.com>
2021-08-31 13:43:34 +0200

    add withdraw tests

--- cypress/integration/03-transaction.spec.js
+++ cypress/integration/03-transaction.spec.js
@@ -5,12 +5,12 @@ const confirmWallet = () => {
     cy.confirmAddress();
 }
 
-describe('Transaction tests', () => {
+const switchToWithdrawPage = () => {
+    cy.get('.sell-page-link').click();
+    cy.get('h1').contains('Withdraw').should('be.visible');
+}
 
-    before(() => {
-        cy.wait(2000);
-        cy.setupSpire();
-    })
+describe('Transaction tests', () => {
 
     beforeEach(() => {
         cy.intercept(
@@ -31,11 +31,12 @@ describe('Transaction tests', () => {
 
     describe('Fund tests', () => {
 
-        it('should fund some tokens', () => {
+        beforeEach(() => {
             cy.visit('/fund-withdraw');
-
             confirmWallet();
+        })
 
+        it('should fund some tokens', () => {
             cy.get('input').type(10);
             cy.get('button[type=submit]').contains('Buy').click();
 
@@ -48,10 +49,6 @@ describe('Transaction tests', () => {
         })
 
         it('should throw an invalid amount error', () => {
-            cy.visit('/fund-withdraw');
-
-            confirmWallet();
-
             cy.get('input').type(0);
             cy.get('button[type=submit]').contains('Buy').click();
 
@@ -60,17 +57,13 @@ describe('Transaction tests', () => {
         })
 
         it('should throw a operation canceled error', () => {
-            cy.visit('/fund-withdraw');
-
-            confirmWallet();
-
             cy.get('input').type(10);
             cy.get('button[type=submit]').contains('Buy').click();
 
             cy.wait(5000);
 
             cy.get('.transaction-creating-modal').should('be.visible');
-            
+
             cy.cancelTransaction();
 
             cy.get('.transaction-error-modal', { timeout: 12000 }).should('be.visible');
@@ -79,7 +72,47 @@ describe('Transaction tests', () => {
     })
 
     describe('Withdraw tests', () => {
-        
+
+        beforeEach(() => {
+            cy.visit('/fund-withdraw');
+            confirmWallet();
+            cy.wait(5000); // wait for notification to disappear
+            switchToWithdrawPage();
+        })
+
+        it('should fund some tokens', () => {
+            cy.get('input').type(10);
+            cy.get('button[type=submit]').contains('Sell').click();
+
+            cy.get('.transaction-creating-modal').should('be.visible');
+            cy.wait(5000);
+            cy.confirmTransaction();
+
+            cy.get('.transaction-processing-modal', { timeout: 12000 }).should('be.visible');
+            cy.get('.transaction-confirmed-modal', { timeout: 10000 }).should('be.visible');
+        })
+
+        it('should throw an invalid amount error', () => {
+            cy.get('input').type(0);
+            cy.get('button[type=submit]').contains('Sell').click();
+
+            cy.get('.transaction-creating-modal').should('be.visible');
+            cy.get('.transaction-error-modal', { timeout: 12000 }).should('be.visible');
+        })
+
+        it('should throw a operation canceled error', () => {
+            cy.get('input').type(10);
+            cy.get('button[type=submit]').contains('Sell').click();
+
+            cy.wait(5000);
+
+            cy.get('.transaction-creating-modal').should('be.visible');
+
+            cy.cancelTransaction();
+
+            cy.get('.transaction-error-modal', { timeout: 12000 }).should('be.visible');
+        })
+
     })
 
     describe('Transaction list tests', () => {
