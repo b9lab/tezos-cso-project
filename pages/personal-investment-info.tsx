d9079e69174fcb072b883f22bb4ff274bfc126ac
Gioele Bonifetto <gioele@b9lab.com>
2021-09-02 17:02:20 +0200

    improves cta card

--- pages/personal-investment-info.tsx
+++ pages/personal-investment-info.tsx
@@ -42,7 +42,7 @@ export default function PersonalInvestmentInfo() {
                         <h1><TezAmount amount={data?.tezInvested}/></h1>
                     </div>
                 </div>
-                <CtaCard/>
+                <CtaCard href="/fund-withdraw" text="Buy TZM &#8594;" label="Invest in TZMINT"/>
             </div>
             {
                 transactionList?.length > 0 &&
--- src/components/CtaCard.tsx
+++ src/components/CtaCard.tsx
@@ -1,14 +1,20 @@
 import Link from "next/link";
 import React from "react";
 
-function CtaCard() {
+type CtaCardProps = {
+    href: string,
+    text: string,
+    label: string
+};
+
+function CtaCard(props: CtaCardProps) {
 
     return (
         <div className="w-full flex-grow sm:max-w-1/2 ">
-            <Link href="/fund-withdraw">
+            <Link href={props.href}>
                 <div className="bg-white rounded shadow-2xl flex flex-col p-4 mt-4 border border-accent-1 cursor-pointer">
-                    <p>Invest in TZMINT</p>
-                    <h2 className="text-accent-1 my-2">Buy TZM &#8594;</h2>
+                    <p>{props.label}</p>
+                    <h2 className="text-accent-1 my-2">{props.text}</h2>
                 </div>
             </Link>
         </div>
