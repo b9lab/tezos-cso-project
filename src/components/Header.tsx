aeb4cafedd0cbd865e33464b0f0c614f4772ac03
Gioele Bonifetto <gioele@b9lab.com>
2021-09-08 16:34:34 +0200

    fixes ticker position

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { NAV_ITEMS } from "../constants"
 
 function Header() {
     return (
-        <div className="text-gray-500 px-6 bg-white font-medium">
+        <div className="text-gray-500 px-8 bg-white font-medium">
             <Nav items={NAV_ITEMS}/>
         </div>
     );
--- src/components/PriceBadge.tsx
+++ src/components/PriceBadge.tsx
@@ -9,7 +9,7 @@ type PriceBadgeProps = {
 function PriceBadge(props: PriceBadgeProps) {
     return (
         <Link href="/fund-withdraw">
-            <div className="price-badge absolute top-20 right-10 bg-light-gray p-2 cursor-pointer">
+            <div className="price-badge absolute top-8 left-8 sm:left-auto sm:top-20 sm:right-10 bg-light-gray p-2 cursor-pointer">
                 <h3 className="text-accent-1">TZM price: {format_tez(props.value ?? 0)} tez</h3>
             </div>
         </Link>
