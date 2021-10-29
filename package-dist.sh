e7ae5282a6c120942affebcd02bb8cdf52c23a05
Gioele Bonifetto <gioele@b9lab.com>
2021-10-29 15:10:47 +0200

    adds public folder to dist

--- package-dist.sh
+++ package-dist.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-zip -r ./dist/dist.zip build database tezos-app-project knexfile.ts next-env.d.ts next.config.js package-lock.json package.json postcss.config.js server.js tailwind.config.js tsconfig.json tsconfig.knex.json
+zip -r ./dist/dist.zip build public database tezos-app-project knexfile.ts next-env.d.ts next.config.js package-lock.json package.json postcss.config.js server.js tailwind.config.js tsconfig.json tsconfig.knex.json
