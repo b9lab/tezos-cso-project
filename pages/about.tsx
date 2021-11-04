c2557b47e80039f2459bb191b2d2d70219da096c
Damien <damien@b9lab.com>
2021-11-04 14:11:31 +0000

    Change of terminology to "Rolling SAFE"

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,14 +3,14 @@ import pricesCurveImage from '../public/buy-sell-function.png';
 import organigramImage from '../public/organigram.png';
 
 /**
- * Programmable Equity page
+ * Rolling SAFE page
  */
 export default function About() {
     return (
         <div className="pb-8">
 
             <div className="pt-32 pb-8 px-8 bg-gradient-to-b from-light-gray to-gray-300">
-                <h1 className="mb-8">Programmable Equity</h1>
+                <h1 className="mb-8">Rolling SAFE</h1>
                 <div className="body-text-large italic">
                     Page removed
                 </div>
